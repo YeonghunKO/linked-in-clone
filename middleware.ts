d9f0718b3ec9138a8c4c0dbe3157fdcc0d580b36
YeonghunKO <yhko1988@gmail.com>
2022-09-27 14:13:59 +0900

    Oauth 연결 성공!
    config-matcher를 잘 설정해줘야함

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+// This function can be marked `async` if using `await` inside
+
+export async function middleware(request: NextRequest) {
+  const { pathname, origin } = request.nextUrl;
+
+  if (pathname === '/') {
+    const session = await getToken({
+      req: request,
+      secret: process.env.JWT_SECRET,
+      secureCookie: process.env.NODE_ENV === 'production',
+    });
+    if (!session) {
+      return NextResponse.redirect(`${origin}/home`);
+    }
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/',
+};
