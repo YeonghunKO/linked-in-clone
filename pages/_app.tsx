c8b3e2800359c91b8fd2b1a58bf06cdda859c098
YeonghunKO <yhko1988@gmail.com>
2022-09-28 22:34:22 +0900

    session provider ready!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: {
+  Component: any;
+  pageProps: any;
+}) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
