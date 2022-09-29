61aee66d092fd8d2be09e154fddbf142caffd4a7
YeonghunKO <yhko1988@gmail.com>
2022-09-29 21:12:18 +0900

    dropIn modal 완료

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,9 +5,14 @@ import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual
 import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import ArticleIcon from '@mui/icons-material/Article';
+import { useRecoilState } from 'recoil';
+import { modalState, modalTypeState } from '../atoms/modalAtoms';
 
 function Input() {
   const { data: session } = useSession();
+  const [modalOpen, setModalOpen] = useRecoilState<boolean>(modalState);
+  const [modalType, setModalType] = useRecoilState<string>(modalTypeState);
+
   return (
     <div className="bg-white dark:bg-[#1D2226] rounded-lg p-3 space-y-3 border border-gray-300 dark:border-none">
       <div className="flex items-center space-x-2">
@@ -16,6 +21,10 @@ function Input() {
           className="!h-10 !w-10 cursor-pointer"
         />
         <motion.button
+          onClick={() => {
+            setModalOpen(true);
+            setModalType('dropIn');
+          }}
           whileHover={{ scale: 1.01 }}
           whileTap={{ scale: 0.99 }}
           className="rounded-full border border-gray-400 py-2.5 px-3 opacity-80 hover:opacity-100 font-medium w-full text-left"
