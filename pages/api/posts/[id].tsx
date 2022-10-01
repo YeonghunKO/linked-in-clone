8249d0f50c76ca3d90984c9ece648900bcc756f2
YeonghunKO <yhko1988@gmail.com>
2022-10-01 10:12:03 +0900

    delete api done!

--- pages/api/posts/[id].tsx
+++ pages/api/posts/[id].tsx
@@ -13,14 +13,14 @@ export default async function handler(
 
   const { db, client } = await connectToDatabase();
 
-  //   if (method === "DELETE") {
-  //     try {
-  //       await db.collection("posts").deleteOne({ _id: new ObjectId(id) });
-  //       res.status(200).json({ message: "The post has been deleted!!" });
-  //     } catch (error) {
-  //       res.status(500).json(error);
-  //     } finally {
-  // client.close()
-  // }
-  //   }
+  if (method === 'DELETE') {
+    try {
+      await db.deleteOne({ _id: new ObjectId(id as string) });
+      res.status(200).json({ message: 'The post has been deleted!!' });
+    } catch (error) {
+      res.status(500).json(error);
+    } finally {
+      client.close();
+    }
+  }
 }
