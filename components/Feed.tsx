83d3689b81b9fe5d5212058bf79a5e7f3154bc53
YeonghunKO <yhko1988@gmail.com>
2022-10-01 10:12:30 +0900

    only render feed component when handle post is true

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -11,22 +11,23 @@ function Feed({ posts }: { posts: PostType[] }) {
   const [useSSRPosts, setUseSSRPosts] =
     useRecoilState<boolean>(useSSRPostsState);
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await fetch('/api/posts', {
-  //       method: 'GET',
-  //       headers: { 'Content-Type': 'application/json' },
-  //     });
-  //     const responseData = await response.json();
-  //     setRealtimePosts(responseData);
-  //     // 여기서 setHandle을 해버리면 두번 랜더링 되는거 아닌가?
-  //     // 왜냐면 Modal - Form 컴포넌트에서 이미 setHandlePost를 했으니깐...
-  //     setHandlePost(false);
-  //     // 이거는 useEffect따로 빼서 한번만 실행되게끔 해야겠는데
-  //     setUseSSRPosts(false);
-  //   };
-  //   fetchPosts();
-  // }, [handlePost]);
+  useEffect(() => {
+
+    const fetchPosts = async () => {
+      const response = await fetch('/api/posts', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const responseData = await response.json();
+      setRealtimePosts(responseData);
+      setHandlePost(false);
+      setUseSSRPosts(false);
+    };
+
+    if (handlePost) {
+      fetchPosts();
+    }
+  }, [handlePost]);
 
   return (
     <div className="space-y-6 pb-24 max-w-lg">
